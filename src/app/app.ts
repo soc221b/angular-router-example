b13825542d4fd9405009923e9de4b3cc41db262e
Ernest <soc221b.e@gmail.com>
2025-11-24 12:11:27 +0800

    add code preview

--- src/app/app.ts
+++ src/app/app.ts
@@ -20,4 +20,34 @@ export class App {
   protected readonly routeReview = routeReview;
   protected readonly routeTeam = routeTeam;
   protected readonly routeUser = routeUser;
+
+  protected readonly code = `
+import { Component, computed, input } from '@angular/core';
+import { Route, RouterLink } from '@angular/router';
+import { RoutesParameters } from './route-parameters';
+
+@Component({
+  selector: 'app-type-safe-router-link',
+  templateUrl: './type-safe-router-link.html',
+  imports: [RouterLink],
+})
+export class TypeSafeRouterLink<T extends [Route, ...Route[]]> {
+  readonly routes = input.required<T>();
+
+  readonly parameters = input.required<RoutesParameters<T>>();
+
+  protected readonly routerLink = computed(() =>
+    this.routes()
+      .map((route) => route.path)
+      .map((path) =>
+        Object.entries(this.parameters()).reduce(
+          (path, [key, value]) => path?.replace(':' + key, value),
+          path,
+        ),
+      )
+      .join('/'),
+  );
+
+  protected readonly title = computed(() => this.routes()[this.routes().length - 1]['title']);
+}`.trim();
 }
